5ec51deb5081b0ebfe99868c085932d7218481af
dolvin17 <huertas876@gmail.com>
2022-09-16 20:11:21 +0200

    100% functional

---
+++ level_00/ft_print_numbers/ft_print_numbers.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/16 20:01:52 by dolvin17          #+#    #+#             */
+/*   Updated: 2022/09/16 20:05:42 by dolvin17         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*Assignment name  : ft_print_numbers
+Expected files   : ft_print_numbers.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a function that displays all digits in ascending order.
+
+Your function must be declared as follows:
+
+void	ft_print_numbers(void);*/
+
+#include <unistd.h>
+
+void	ft_print_numbers(void)
+{
+	char	digit;
+
+	digit = '0';
+	while (digit <= '9')
+	{
+		write(1, &digit, 1);
+		digit++;
+	}
+}
+/*
+int	main(void)
+{
+	ft_print_numbers();
+	return (0);
+}*/
