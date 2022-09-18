5e8399a5192bf132c689e7cfc15aef44df60a929
dolvin17 <huertas876@gmail.com>
2022-09-18 11:35:42 +0200

    100% functional

---
+++ level_02/inter/inter.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   inter.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/18 11:25:48 by dolvin17          #+#    #+#             */
+/*   Updated: 2022/09/18 11:35:01 by dolvin17         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*Assignment name  : inter
+Expected files   : inter.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+Write a program that takes two strings and displays, without doubles, the
+characters that appear in both strings, in the order they appear in the first
+one.
+The display will be followed by a \n.
+If the number of arguments is not 2, the program displays \n.
+Examples:
+$>./inter "padinton" "paqefwtdjetyiytjneytjoeyjnejeyj" | cat -e
+padinto$
+$>./inter ddf6vewg64f gtwthgdwthdwfteewhrtag6h4ffdhsd | cat -e
+df6ewg4$
+$>./inter "rien" "cette phrase ne cache rien" | cat -e
+rien$
+$>./inter | cat -e
+$*/
+
+#include <unistd.h>
+
+int	iter(char *str, char c, int len)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] && (i < len || len == -1))
+		if (str[i++] == c)
+			return (1);
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	if (argc == 3)
+	{
+		i = 0;
+		while (argv[1][i])
+		{
+			if (!iter(argv[1], argv[1][i], i) && iter(argv[2], argv[1][i], -1))
+				write(1, &argv[1][i], 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
