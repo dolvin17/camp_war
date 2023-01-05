dadad7ec169ced23d792ab00a033e5641618f329
dolvin17 <huertas876@gmail.com>
2023-01-05 21:17:51 +0100

    100% functional

--- level_02/ft_strdup/ft_strdup.c
+++ level_02/ft_strdup/ft_strdup.c
@@ -5,75 +5,48 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/17 22:27:14 by dolvin17          #+#    #+#             */
-/*   Updated: 2022/09/17 23:06:40 by dolvin17         ###   ########.fr       */
+/*   Created: 2023/01/05 20:47:37 by dolvin17          #+#    #+#             */
+/*   Updated: 2023/01/05 21:03:33 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*voAssignment name  : ft_strdup
-Expected files   : ft_strdup.c
-Allowed functions: malloc
---------------------------------------------------------------------------------
-Reproduce the behavior of the function strdup (man strdup).
-Your function must be declared as follows:
-
-char    *ft_strdup(char *src);*/
-
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
-int	ft_strlen(char *str)
+int	ft_strlen(char *src)
 {
 	int	i;
 
 	i = 0;
-	while (str[i])
+	while (src[i] != '\0')
 		i++;
 	return (i);
 }
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	const unsigned char	*src_pointer;
-	unsigned char		*dest_pointer;
+	unsigned char	*new_dest;
+	unsigned const char	*new_src;
 
-	if (!dest && !src)
-		return (0);
-	dest_pointer = dest;
-	src_pointer = src;
-	while (n)
+	new_dest = dst;
+	new_src = src;
+	while (n--)
 	{
-		n--;
-		*dest_pointer++ = *src_pointer++;
+		*new_dest++ = *new_src++;
 	}
-	return (dest);
+	return (dst);
 }
 
 char	*ft_strdup(char *src)
 {
-	char	*memory_reserve;
+	char	*dest;
 	int		len;
 
 	len = ft_strlen(src);
-	memory_reserve = malloc(sizeof(char) * (len + 1));
-	if (!memory_reserve)
+	dest = (char *)malloc(sizeof(char) * (len + 1));
+	if (!dest)
 		return (NULL);
-	ft_memcpy(memory_reserve, src, len);
-	memory_reserve[len] = '\0';
-	return (memory_reserve);
+	ft_memcpy(dest, src, len);
+	dest[len] = '\0';
+	return (dest);
 }
-/*
-int	main(void)
-{
-	const char	*str;
-	const char	*test;
-
-	str = ft_strdup("Marvin");
-	test = strdup("Marvin");
-	printf("ft_strdup: %s\n", str);
-	printf("strdup: %s\n", test);
-	//free(str);
-	system("leaks a.out");
-	return (0);
-}*/
