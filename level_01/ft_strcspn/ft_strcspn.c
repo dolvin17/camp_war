2d41ef9af0bd30b65c4453a8f60772d5666b1b82
dolvin17 <huertas876@gmail.com>
2023-01-08 18:43:39 +0100

    100% functional

--- level_01/ft_strcspn/ft_strcspn.c
+++ level_01/ft_strcspn/ft_strcspn.c
@@ -3,28 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcspn.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ghuertas <ghuertas@student.42madrid>       +#+  +:+       +#+        */
+/*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/06 12:34:29 by ghuertas          #+#    #+#             */
-/*   Updated: 2022/12/06 13:18:18 by ghuertas         ###   ########.fr       */
+/*   Created: 2023/01/08 18:23:37 by dolvin17          #+#    #+#             */
+/*   Updated: 2023/01/08 18:42:23 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
 #include <stdio.h>
-
+#include <string.h>
 /*Cuenta el número de chars de un string inicial (str) (hasta el indice de reject),
 siempre que no contenga ninguno de los caracteres existentes en (reject).
-
 Return value: Si no hay coincidencias, retorna la longitud total de str
 Return value: Si reject, es un string vacío, retorna la longitud total de str
 Return value: Si reject, es un string NULL, el comportamiento es indefinido.
 Retunr value: Si la coincidencia es completa, (str == reject) retorna 0 */
-
 size_t	ft_strcspn(const char *str, const char *reject)
 {
-	int i;
-	int j;
+	int	i;
+	int	j;
 
 	i = 0;
 	while (str[i] != '\0')
@@ -34,20 +31,18 @@ size_t	ft_strcspn(const char *str, const char *reject)
 		{
 			if (str[i] == reject[j])
 				return (i);
-			j++;
+			else
+				j++;
 		}
 		i++;
 	}
-	i = 0;
-	while (str[i] != '\0')
-		i++;
 	return (i);
 }
 /*
 int main (void)
 {
-	printf("My own function: %zu\n", ft_strcspn("holamundo", "m"));
-	printf("The Original one: %lu\n", strcspn("holamundo", "m"));
+	printf("My own function: %zu\n", ft_strcspn("holamundo", "mundo"));
+	printf("The Original one: %lu\n", strcspn("holamundo", "mundo"));
 	printf("My own function: %zu\n", ft_strcspn("Marvin", "x"));
 	printf("The Original one: %lu\n", strcspn("Marvin", "x"));
 	printf("My own function: %zu\n", ft_strcspn("Gatito", "o"));
