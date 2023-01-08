92a714d2ed84953d6b9b6940be05a762da7e1bec
dolvin17 <huertas876@gmail.com>
2023-01-08 10:04:43 +0100

    improved

--- level_02/ft_strdup/ft_strdup.c
+++ level_02/ft_strdup/ft_strdup.c
@@ -5,36 +5,42 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/05 20:47:37 by dolvin17          #+#    #+#             */
-/*   Updated: 2023/01/05 21:03:33 by dolvin17         ###   ########.fr       */
+/*   Created: 2023/01/08 09:43:08 by dolvin17          #+#    #+#             */
+/*   Updated: 2023/01/08 10:02:32 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+/* Assignment name  : ft_strdup
+Expected files   : ft_strdup.c
+Allowed functions: malloc
+--------------------------------------------------------------------------------
+Reproduce the behavior of the function strdup (man strdup).
+Your function must be declared as follows:
+char    *ft_strdup(char *src);*/
 #include <stdlib.h>
 
-int	ft_strlen(char *src)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	int	i;
-
-	i = 0;
-	while (src[i] != '\0')
-		i++;
-	return (i);
-}
-
-void	*ft_memcpy(void *dst, const void *src, size_t n)
-{
-	unsigned char	*new_dest;
+	unsigned char		*new_dest;
 	unsigned const char	*new_src;
 
-	new_dest = dst;
+	new_dest = dest;
 	new_src = src;
 	while (n--)
 	{
 		*new_dest++ = *new_src++;
 	}
-	return (dst);
+	return (dest);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
 }
 
 char	*ft_strdup(char *src)
@@ -43,10 +49,22 @@ char	*ft_strdup(char *src)
 	int		len;
 
 	len = ft_strlen(src);
-	dest = (char *)malloc(sizeof(char) * (len + 1));
+	dest = (char *)malloc(sizeof (char) * (len + 1));
 	if (!dest)
 		return (NULL);
 	ft_memcpy(dest, src, len);
 	dest[len] = '\0';
 	return (dest);
 }
+/*
+#include <stdio.h>
+#include <string.h>
+
+int	main()
+{
+	char	src[]="hola marvin";
+	
+	printf("%s\n", ft_strdup(src));
+	printf("%s\n", strdup(src));
+	return (0);
+}*/
