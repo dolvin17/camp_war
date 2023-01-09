dda99e7c0c01edd0b0aead732ee17b29c99cc763
dolvin17 <huertas876@gmail.com>
2023-01-09 04:43:41 +0100

    100% functional, improved

--- level_02/max/max.c
+++ level_02/max/max.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/06 16:53:55 by dolvin17          #+#    #+#             */
-/*   Updated: 2023/01/06 17:18:19 by dolvin17         ###   ########.fr       */
+/*   Created: 2023/01/09 04:16:18 by dolvin17          #+#    #+#             */
+/*   Updated: 2023/01/09 04:41:02 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,30 +20,31 @@ The first parameter is an array of int, the second is the number of elements in
 the array.
 The function returns the largest number found in the array.
 If the array is empty, the function returns 0.*/
+#include <limits.h>
 
-#include <stdio.h>
-
-int		max(int* tab, unsigned int len)
+int	max(int* tab, unsigned int len)
 {
 	unsigned int	i;
-	int position;
+	int largest;
 
 	i = 0;
+	largest = INT_MIN;
 	if (len == 0)
 		return (0);
-	position = tab[i];
-	while (i < len)
+	while (i <= len)
 	{
-		if (tab[i] > position)
-			position = tab[i];
+		if (tab[i] > largest)
+			largest = tab[i];
 		i++;
 	}
-	return (position);
+	return (largest);
 }
 /*
+#include <stdio.h>
 int main()
 {
-	int	tab[] = {-1, 5, -7, 7};
-	printf("%d\n", max(tab, 0));
+	int	tab[]={1, 3, 5, 2, 4};
+	int len = 4;
+	printf("%d\n", max(tab, len));
 	return (0);
 }*/
