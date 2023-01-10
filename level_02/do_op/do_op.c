c2d37538fc79a583a3fd20c78c84bb6b82049c56
dolvin17 <huertas876@gmail.com>
2023-01-10 16:18:53 +0100

    improved

--- level_02/do_op/do_op.c
+++ level_02/do_op/do_op.c
@@ -5,59 +5,36 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/18 17:05:04 by dolvin17          #+#    #+#             */
-/*   Updated: 2022/09/18 17:31:41 by dolvin17         ###   ########.fr       */
+/*   Created: 2023/01/10 12:51:56 by dolvin17          #+#    #+#             */
+/*   Updated: 2023/01/10 16:17:00 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*Assignment name  : do_op
-Expected files   : *.c, *.h
-Allowed functions: atoi, printf, write
---------------------------------------------------------------------------------
-Write a program that takes three strings:
-- The first and the third one are representations of base-10 signed integers
-that fit in an int.
-- The second one is an arithmetic operator chosen from: + - * / %
-The program must display the result of the requested arithmetic operation,
-followed by a newline. If the number of parameters is not 3, the program
-just displays a newline.
-You can assume the string have no mistakes or extraneous characters. Negative
-numbers, in input or output, will have one and only one leading '-'. The
-result of the operation fits in an int.
-Examples:
-$> ./do_op "123" "*" 456 | cat -e
-56088$
-$> ./do_op "9828" "/" 234 | cat -e
-42$
-$> ./do_op "1" "+" "-43" | cat -e
--42$
-$> ./do_op | cat -e
-$*/
-
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
-
-int	do_operation(int number_a, int number_b, char sign)
-{
-	if (sign == '*')
-		return (number_a * number_b);
-	else if (sign == '+')
-		return (number_a + number_b);
-	else if (sign == '-')
-		return (number_a - number_b);
-	else if (sign == '/')
-		return (number_a / number_b);
-	else if (sign == '%')
-		return (number_a % number_b);
-	return (0);
-}
 
 int	main(int argc, char **argv)
 {
+	int	i;
+	int argv1 = 0;
+	int argv3 = 0;
+	
+	i = 0;
 	if (argc == 4)
 	{
-		printf("%d\n", do_operation(atoi(argv[1]), atoi(argv[3]), (argv[2][0])));
+		argv1 = (atoi(argv[1]));
+		argv3 = (atoi(argv[3]));
+		if (argv[2][i] == '*')
+			printf("%d\n", (argv1 * argv3));
+		if (argv[2][i] == '+')
+			printf("%d\n", (argv1 + argv3));
+		if (argv[2][i] == '-')
+			printf("%d\n", (argv1 - argv3));
+		if (argv[2][i] == '/')
+			printf("%d\n", (argv1 / argv3));
+		if (argv[2][i] == '%')
+			printf("%d\n", (argv1 % argv3));
 	}
 	else
 		write(1, "\n", 1);
