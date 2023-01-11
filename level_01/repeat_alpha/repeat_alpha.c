809e9893810852c13fb3d5f8695f3d5467b9a5a8
dolvin17 <huertas876@gmail.com>
2023-01-11 20:28:21 +0100

    100% functional

--- level_01/repeat_alpha/repeat_alpha.c
+++ level_01/repeat_alpha/repeat_alpha.c
@@ -5,82 +5,46 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/17 15:52:56 by dolvin17          #+#    #+#             */
-/*   Updated: 2022/09/17 16:18:44 by dolvin17         ###   ########.fr       */
+/*   Created: 2023/01/11 20:02:43 by dolvin17          #+#    #+#             */
+/*   Updated: 2023/01/11 20:24:46 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*Assignment name  : repeat_alpha
-Expected files   : repeat_alpha.c
-Allowed functions: write
---------------------------------------------------------------------------------
-Write a program called repeat_alpha that takes a string and display it
-repeating each alphabetical character as many times as its alphabetical index,
-followed by a newline.
-'a' becomes 'a', 'b' becomes 'bb', 'e' becomes 'eeeee', etc...
-Case remains unchanged.
-If the number of arguments is not 1, just display a newline.
-Examples:
-$>./repeat_alpha "abc"
-abbccc
-$>./repeat_alpha "Alex." | cat -e
-Alllllllllllleeeeexxxxxxxxxxxxxxxxxxxxxxxx.$
-$>./repeat_alpha 'abacadaba 42!' | cat -e
-abbacccaddddabba 42!$
-$>./repeat_alpha | cat -e
-$
-$>
-$>./repeat_alpha "" | cat -e
-$
-$>*/
-
 #include <unistd.h>
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-char	check_is_alpha(char *str)
+int	main(int argc, char **argv)
 {
 	int	i;
 	int	j;
 
 	i = 0;
-	while (str[i])
-	{
-		if (str[i] >= 'A' && str[i] <= 'Z')
-		{
-			j = str[i] - 65;
-			while (j >= 0)
-			{
-				ft_putchar(str[i]);
-				j--;
-			}
-		}
-		else if (str[i] >= 'a' && str[i] <= 'z')
-		{
-			j = str[i] - 97;
-			while (j >= 0)
-			{
-				ft_putchar(str[i]);
-				j--;
-			}
-		}
-		else
-			ft_putchar(str[i]);
-		i++;
-	}
-	ft_putchar('\n');
-	return (*str);
-}
-int	main(int argc, char **argv)
-{
 	if (argc == 2)
 	{
-		check_is_alpha(argv[1]);
+		while (argv[1][i] && argv[1][i] != '\0')
+		{
+			if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
+			{
+				j = argv[1][i] - 'A';
+				while (j >= 0)
+				{
+					write(1, &argv[1][i], 1);
+					j--;
+				}
+			}
+			else if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
+			{
+				j = argv[1][i] - 'a';
+				while (j >= 0)
+				{
+					write(1, &argv[1][i], 1);
+					j--;
+				}
+			}
+			else
+				write(1, &argv[1][i], 1);
+			i++;
+		}
 	}
-	else
-		ft_putchar('\n');
+	write(1, "\n", 1);
 	return (0);
 }
