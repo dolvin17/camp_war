4b4c1e7fc9de7b152986a523f61b16611cb276b9
dolvin17 <huertas876@gmail.com>
2023-01-16 22:38:58 +0100

    shortest

--- level_02/print_hex/print_hex.c
+++ level_02/print_hex/print_hex.c
@@ -5,60 +5,44 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/06 17:24:02 by dolvin17          #+#    #+#             */
-/*   Updated: 2023/01/06 17:56:46 by dolvin17         ###   ########.fr       */
+/*   Created: 2023/01/16 22:16:24 by dolvin17          #+#    #+#             */
+/*   Updated: 2023/01/16 22:36:18 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*Assignment name  : print_hex
-Expected files   : print_hex.c
-Allowed functions: write
---------------------------------------------------------------------------------
-Write a program that takes a positive (or zero) number expressed in base 10,
-and displays it in base 16 (lowercase letters) followed by a newline.
-If the number of parameters is not 1, the program displays a newline.
-Examples:
-$> ./print_hex "10" | cat -e
-a$
-$> ./print_hex "255" | cat -e
-ff$
-$> ./print_hex "5156454" | cat -e
-4eae66$
-$> ./print_hex | cat -e
-$*/
 #include <unistd.h>
 
 int	ft_atoi(char *str)
 {
-	int	n;
+	int	nbr;
 	int i;
 
-	n = 0;
 	i = 0;
+	nbr = 0;
 	while (str[i] != '\0')
 	{
-		n = n * 10;
-		n = n + str[i] - '0';
+		nbr = nbr * 10;
+		nbr = nbr + str[i] - '0';
 		i++;
 	}
-	return (n);
+	return (nbr);
 }
 
-void	print_hex(int n)
+void putnbr_hex(int nbr)
 {
-	char	*base;
+	char *base = "0123456789abcdef";
 	
-	base = "0123456789abcdef";
-	if (n >= 16)
-		print_hex(n / 16);
-	write(1, &base[n % 16], 1);
+	if (nbr >= 16)
+		putnbr_hex(nbr / 16);
+	write(1, &base[nbr % 16], 1);
 }
 
 int	main(int argc, char **argv)
 {
 	if (argc == 2)
 	{
-		print_hex(ft_atoi(argv[1]));
+		putnbr_hex(ft_atoi(argv[1]));
 	}
 	write(1, "\n", 1);
+	return (0);
 }
