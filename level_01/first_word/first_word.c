8c47163d5fa0b8c0a3421edc746812db836a4b7b
dolvin17 <huertas876@gmail.com>
2023-01-16 15:13:49 +0100

    improved

--- level_01/first_word/first_word.c
+++ level_01/first_word/first_word.c
@@ -5,27 +5,46 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/07 11:27:39 by dolvin17          #+#    #+#             */
-/*   Updated: 2022/12/07 12:14:21 by dolvin17         ###   ########.fr       */
+/*   Created: 2023/01/16 15:03:17 by dolvin17          #+#    #+#             */
+/*   Updated: 2023/01/16 15:12:43 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*Assignment name  : first_word
+Expected files   : first_word.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+Write a program that takes a string and displays its first word, followed by a
+newline.
+A word is a section of string delimited by spaces/tabs or by the start/end of
+the string.
+If the number of parameters is not 1, or if there are no words, simply display
+a newline.
+Examples:
+$> ./first_word "FOR PONY" | cat -e
+FOR$
+$> ./first_word "this        ...       is sparta, then again, maybe    not" | cat -e
+this$
+$> ./first_word "   " | cat -e
+$
+$> ./first_word "a" "b" | cat -e
+$
+$> ./first_word "  lorem,ipsum  " | cat -e
+lorem,ipsum$
+$>*/
+
 #include <unistd.h>
 
-int main (int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	int i;
+	int	i = 0;
 
-	i = 0;
 	if (argc == 2)
 	{
-		while (argv[1][i] && argv[1][i] == ' ')
+		while (argv[1][i] == ' ')
 			i++;
-		while (argv[1][i] != '\0' && argv[1][i] != ' ')
-		{
-			write(1, &argv[1][i], 1);
-			i++;
-		}
+		while (argv[1][i] != ' ' && argv[1][i])
+			write(1, &argv[1][i++], 1);
 	}
 	write(1, "\n", 1);
 	return (0);
