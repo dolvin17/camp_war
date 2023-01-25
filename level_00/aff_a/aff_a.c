2a5d3b031de5bef0b4517448e523923b1309f1ad
dolvin17 <huertas876@gmail.com>
2023-01-25 15:04:06 +0100

    improved

--- level_00/aff_a/aff_a.c
+++ level_00/aff_a/aff_a.c
@@ -1,40 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   aff_a.c                                            :+:      :+:    :+:   */
+/*   maff_a.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/16 18:32:00 by dolvin17          #+#    #+#             */
-/*   Updated: 2022/10/06 23:35:28 by dolvin17         ###   ########.fr       */
+/*   Created: 2023/01/25 14:07:06 by dolvin17          #+#    #+#             */
+/*   Updated: 2023/01/25 14:10:12 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*Assignment name  : aff_a
-Expected files   : aff_a.c
-Allowed functions: write
---------------------------------------------------------------------------------
-Write a program that takes a string, and displays the first 'a' character it
-encounters in it, followed by a newline. If there are no 'a' characters in the
-string, the program just writes a newline. If the number of parameters is not
-1, the program displays 'a' followed by a newline.
-Example:
-$> ./aff_a "abc" | cat -e
-a$
-$> ./aff_a "RaInB0w d4Sh!" | cat -e
-a$
-$> ./aff_a "ThE Ck3 Is L|3" | cat -e
-$
-$> ./aff_a | cat -e
-a$*/
-
 #include <unistd.h>
-
-#include <unistd.h>
-
 int	main(int argc, char **argv)
 {
-	int	i;
+	int i;
 
 	i = 0;
 	if (argc == 2)
@@ -42,15 +21,9 @@ int	main(int argc, char **argv)
 		while (argv[1][i])
 		{
 			if (argv[1][i] == 'a')
-			{
-				write(1, &argv[1][i], 1);
-				write(1, "\n", 1);
-			}
-			if (argv[1][i] == 'a')
-				return (0);
+				write(1, "a\n", 1);
 			i++;
 		}
-		write(1, "\n", 1);
 	}
 	else
 		write(1, "a\n", 2);
