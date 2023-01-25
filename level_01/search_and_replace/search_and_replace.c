62e9ffca078e00662edd5525cedac9cee61dda56
dolvin17 <huertas876@gmail.com>
2023-01-25 19:25:10 +0100

    improved

--- level_01/search_and_replace/search_and_replace.c
+++ level_01/search_and_replace/search_and_replace.c
@@ -3,12 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   search_and_replace.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ghuertas <ghuertas@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dolvin17 <grks_17@hotmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/15 11:24:01 by ghuertas          #+#    #+#             */
-/*   Updated: 2022/09/15 12:06:50 by ghuertas         ###   ########.fr       */
+/*   Created: 2023/01/25 19:19:29 by dolvin17          #+#    #+#             */
+/*   Updated: 2023/01/25 19:24:49 by dolvin17         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
 
 /*Assignment name  : search_and_replace
 Expected files   : search_and_replace.c
@@ -33,30 +35,23 @@ $>./search_and_replace "ZoZ eT Dovid oiME le METol." "o" "a" | cat -e
 ZaZ eT David aiME le METal.$
 $>./search_and_replace "wNcOre Un ExEmPle Pas Facilw a Ecrirw " "w" "e" | cat -e
 eNcOre Un ExEmPle Pas Facile a Ecrire $*/
-
-#include <unistd.h>
-
 int	main(int argc, char **argv)
 {
-	int	i;
+	int i;
 	int	j;
 
 	j = 0;
 	i = 0;
 	if (argc == 4)
 	{
-		if (!argv[2][1] && !argv[3][1])
+		while (argv[1][i] && !argv[2][1] && !argv[3][1])
 		{
-			while (argv[1][i] != '\0')
-			{
-				if (argv[1][i] == argv[2][j])
-					argv[1][i] = argv[3][j];
-				write(1, &argv[1][i], 1);
-				i++;
-			}
+			if (argv[1][i] == argv[2][j])
+				argv[1][i] = argv[3][j];
+			write(1, &argv[1][i], 1);
+			i++;
 		}
-		write(1, "\n", 1);
 	}
-	else
-		write(1, "\n", 1);
+	write(1, "\n", 1);
+	return (0);
 }
